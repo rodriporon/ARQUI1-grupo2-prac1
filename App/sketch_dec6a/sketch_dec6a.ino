9894759952b651997a300c64079b14941abae3b1
rodri <3065713820401@ingenieria.usac.edu.gt>
2022-12-07 11:10:18 -0600

    se reciben cadenas de Java en arduino

--- App/sketch_dec6a/sketch_dec6a.ino
+++ App/sketch_dec6a/sketch_dec6a.ino
@@ -1,20 +1,26 @@
-
-String serialText = "";
+#define LED 13
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
 }
 
 void loop() {
-  while(Serial.available()){
-    delay(10);
-    char c = Serial.read();
-      serialText += c;
+  if (Serial.available()) {
     
+    // data será la cadena que se recibirá desde Java
+    String data = Serial.readStringUntil('\n');
+
+
+    if (data == "encender") {
+      digitalWrite(LED, HIGH);
+    }
+
+    if (data == "apagar") {
+      digitalWrite(LED, LOW);
+    }
+
   }
-
-  Serial.println(serialText);  
-
 
 }
